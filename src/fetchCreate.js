90e80960aa77ca0681aa84840a9f7780edf6f14b
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-21 16:16:10 +0300

    0.6+

--- src/fetchCreate.js
+++ src/fetchCreate.js
@@ -1,29 +1,24 @@
 
-
-
 export default class FetchImg {
 
     constructor(searchRequest, options) {
         this.searchRequest = searchRequest;
-        this.pg = 0;
+        this.pg = 0
         this.options = options;
-        this.turnPages()
+
     };
 
-    turnPages() {
+
+    creatingRequest() {
+
         this.pg += 1;
-        const result = this.creatingRequest(this.pg)
-        console.log(result, "in");
-        return result
-    };
 
-    creatingRequest(pageNumber) {
         const url = new URL('https://pixabay.com/api/');
         const optionsArr = Object.entries(this.options);
         optionsArr.forEach(el => url.searchParams.set(el[0], el[1]));
-        url.searchParams.set("page", pageNumber);
+        url.searchParams.set("page", this.pg);
         url.searchParams.set("q", this.searchRequest);
-        return fetch(url.href).then(res => res)
+        return fetch(url.href)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -14,10 +14,17 @@ formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
-    const searchRequest = formEl.elements.query.value;
-    new FetchImg(searchRequest, options);
 
-    console.log(FetchImg.turnPages());
+    const searchRequest = formEl.elements.query.value;
+    const request = new FetchImg(searchRequest, options);
+
+    request.creatingRequest()
+        .then(res => res.json())
+        .then(res => console.log(res))
+
+
+
+
 }
 
 
