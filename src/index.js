65b8324a04615aca4d2c9eb91c09f57e6ff1e146
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-21 22:07:38 +0300

    0.7

--- src/index.js
+++ src/index.js
@@ -3,31 +3,45 @@ import galleryTemplate from "./templates/gallery.hbs";
 import cardTemplate from "./templates/card.hbs";
 import FetchImg from "./fetchCreate";
 import options from "./apiService"
+import { error } from "@pnotify/core";
 
 
 const bodyEl = document.querySelector('body');
 
 
 bodyEl.innerHTML = formTemplate();
+bodyEl.insertAdjacentHTML('beforeend', galleryTemplate());
+
 const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+
 formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
 
+    galleryEl.innerHTML = '';
+
     const searchRequest = formEl.elements.query.value;
     const request = new FetchImg(searchRequest, options);
 
     request.creatingRequest()
+        .then(res => {
+            if (!res.ok) {
+                Promise.reject(res)
+            }
+            return res
+        })
         .then(res => res.json())
-        .then(res => console.log(res))
-
-
-
-
+        .then(res => createMarkup(res))
+        .catch(err => console.log("err", err))
 }
 
+function createMarkup(data) {
 
+    const markup = cardTemplate(data.hits);
+    galleryEl.insertAdjacentHTML('beforeend', markup)
+}
 
 
 
